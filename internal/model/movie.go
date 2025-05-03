17550a82c8429c485b9d752f8a66af88835a9b8f
AbdulBari <bay7amov@gmail.com>
2025-05-03 17:42:17 +0300

    update validate parameter name

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -16,15 +16,18 @@ type Film struct {
 
 // Validate - проверка данных фильма
 func (f *Film) Validate() error {
-	if f.Description == "" || len(f.Description) > 150 {
+	if f.Name == "" || len(f.Name) > 150 {
 		return fmt.Errorf("название фильма должно быть от 1 до 150 символов")
 	}
+
 	if len(f.Description) > 1000 {
 		return fmt.Errorf("описание не должно превышать 1000 символов")
 	}
+
 	if f.Rating < 0 || f.Rating > 10 {
 		return fmt.Errorf("рейтинг должен быть от 0 до 10")
 	}
+
 	return nil
 }
 
